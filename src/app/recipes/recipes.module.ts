66573bde166dcf26140a2f395f7f8995d0fd38ed
Tamás Bosánszki <bosanszkitamas@gmail.com>
2023-01-01 23:09:59 +0100

    introducing feature module - recipes

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { SharedModule } from "../shared/shared.module";
+
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeComponent } from "./recipe-list/recipe/recipe.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipesRoutingModule } from "./recipes-routing.module";
+import { RecipesComponent } from "./recipes.component";
+
+@NgModule({
+    declarations: [
+        RecipesComponent,
+        RecipeListComponent,
+        RecipeDetailComponent,
+        RecipeComponent,
+        RecipeStartComponent,
+        RecipeEditComponent
+    ],
+    imports: [
+        RouterModule,
+        ReactiveFormsModule,
+        RecipesRoutingModule,
+        SharedModule
+    ]
+})
+export class RecipesModule {}
