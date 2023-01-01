8e87a4fd451245ab41cb9d8b475e37e15a9dd29c
Tamás Bosánszki <bosanszkitamas@gmail.com>
2023-01-01 20:26:25 +0100

    authentication service

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,112 @@
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Router } from "@angular/router";
+import { BehaviorSubject, catchError, Subject, tap, throwError } from "rxjs";
+import { UserModel } from "./user.model";
+
+export interface AuthResponseData {
+    kind: string;
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+    registered?: boolean;
+}
+
+
+
+@Injectable({providedIn: 'root'})
+export class AuthService {
+    user = new BehaviorSubject<UserModel>(null);
+    private tokenExpirationTimer: any;
+
+    constructor(private http: HttpClient, private router: Router) {}
+    signup(email: string, password: string){
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDvj-JktWsvEfdI3xMsy68ZJCTt1wtWrRA',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        ).pipe(catchError(this.handleError), tap(resData => {
+            this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+        }));
+    }
+
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDvj-JktWsvEfdI3xMsy68ZJCTt1wtWrRA',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        ).pipe(catchError(this.handleError), tap(resData => {
+            this.handleAuthentication(resData.email, resData.localId, resData.idToken, +resData.expiresIn);
+        }));
+    }
+
+    logout() {
+        this.user.next(null);
+        this.router.navigate(['/auth']);
+        localStorage.removeItem('userData');
+        if(this.tokenExpirationTimer){
+            clearTimeout(this.tokenExpirationTimer);
+        }
+        this.tokenExpirationTimer = null;
+    }
+
+    autoLogout(expirationDuration: number) {
+        this.tokenExpirationTimer = setTimeout(() => {
+            this.logout();
+        }, expirationDuration);
+    }
+
+    autoLogin() {
+        const userData: {
+            email: string;
+            id: string;
+            _token: string;
+            _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'));
+        if(!userData) {
+            return;
+        }
+        
+        const loadedUser = new UserModel(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+        if(loadedUser.token){
+            this.user.next(loadedUser);
+            const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
+            this.autoLogout(expirationDuration);
+        }
+    }
+
+    private handleAuthentication(email: string, userId: string, token: string, expiresIn: number){
+        const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+        const user = new UserModel(email, userId, token, expirationDate);
+        this.user.next(user);
+        this.autoLogout(expiresIn * 1000);
+        localStorage.setItem('userData', JSON.stringify(user));
+    }
+
+    private handleError(errorRes: HttpErrorResponse){
+        let errorMessage = 'An unknown error occured!';
+            if(!errorRes.error || !errorRes.error.error){
+                return throwError(errorMessage);
+            }
+            switch(errorRes.error.error.message){
+                case 'EMAIL_EXISTS':
+                    errorMessage = "This email already exists.";
+                    break;
+                case 'EMAIL_NOT_FOUND':
+                    errorMessage = "This email does not exist.";
+                    break;
+                case 'INVALID_PASSWORD':
+                    errorMessage = "Wrong Password!";
+                    break;
+            }
+            return throwError(errorMessage);
+    }
+}
