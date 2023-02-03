e4f84e270385659caced4941a36204baebd97f59
Tamás Bosánszki <bosanszkitamas@gmail.com>
2023-02-03 10:48:46 +0100

    fetch recipes on load of the endpoint

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -17,13 +17,13 @@ export interface AuthResponseData {
 
 
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class AuthService {
     user = new BehaviorSubject<UserModel>(null);
     private tokenExpirationTimer: any;
 
-    constructor(private http: HttpClient, private router: Router) {}
-    signup(email: string, password: string){
+    constructor(private http: HttpClient, private router: Router) { }
+    signup(email: string, password: string) {
         return this.http.post<AuthResponseData>(
             'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + environment.firebaseAPIKey,
             {
@@ -53,7 +53,7 @@ export class AuthService {
         this.user.next(null);
         this.router.navigate(['/auth']);
         localStorage.removeItem('userData');
-        if(this.tokenExpirationTimer){
+        if (this.tokenExpirationTimer) {
             clearTimeout(this.tokenExpirationTimer);
         }
         this.tokenExpirationTimer = null;
@@ -72,19 +72,19 @@ export class AuthService {
             _token: string;
             _tokenExpirationDate: string;
         } = JSON.parse(localStorage.getItem('userData'));
-        if(!userData) {
+        if (!userData) {
             return;
         }
-        
+
         const loadedUser = new UserModel(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
-        if(loadedUser.token){
+        if (loadedUser.token) {
             this.user.next(loadedUser);
             const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
             this.autoLogout(expirationDuration);
         }
     }
 
-    private handleAuthentication(email: string, userId: string, token: string, expiresIn: number){
+    private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
         const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
         const user = new UserModel(email, userId, token, expirationDate);
         this.user.next(user);
@@ -92,22 +92,22 @@ export class AuthService {
         localStorage.setItem('userData', JSON.stringify(user));
     }
 
-    private handleError(errorRes: HttpErrorResponse){
+    private handleError(errorRes: HttpErrorResponse) {
         let errorMessage = 'An unknown error occured!';
-            if(!errorRes.error || !errorRes.error.error){
-                return throwError(errorMessage);
-            }
-            switch(errorRes.error.error.message){
-                case 'EMAIL_EXISTS':
-                    errorMessage = "This email already exists.";
-                    break;
-                case 'EMAIL_NOT_FOUND':
-                    errorMessage = "This email does not exist.";
-                    break;
-                case 'INVALID_PASSWORD':
-                    errorMessage = "Wrong Password!";
-                    break;
-            }
+        if (!errorRes.error || !errorRes.error.error) {
             return throwError(errorMessage);
+        }
+        switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = "This email already exists.";
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = "This email does not exist.";
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = "Wrong Password!";
+                break;
+        }
+        return throwError(errorMessage);
     }
 }
