1fd8802e1201fc235a6948fe18ad31ed6e4d6576
Tamás Bosánszki <bosanszkitamas@gmail.com>
2023-02-03 10:54:58 +0100

    format fix

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -6,9 +6,9 @@ import { Directive, ElementRef, HostBinding, HostListener } from "@angular/core"
 export class DropdownDirective {
     @HostBinding('class.open') isOpen = false;
 
-    @HostListener('document:click', ['$event']) toggleOpen(event: Event){
+    @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
         this.isOpen = this.elementRef.nativeElement.contains(event.target) ? !this.isOpen : false;
     }
 
-    constructor(private elementRef: ElementRef) {}
+    constructor(private elementRef: ElementRef) { }
 }
